0a64ca29db5fb18f3a8c8aeef5b06d3dba1f6e4f
Juha Nurmi <juha.nurmi@ahmia.fi>
2014-10-27 05:46:17 +0200

    Updates to torrc generation.

--- tools/generate_torrc.py
+++ tools/generate_torrc.py
@@ -5,7 +5,7 @@ import codecs  # UTF-8 support for the text files
 
 def text2file(txt, filename):
     """Write the txt to the file."""
-    outputfile = codecs.open(filename, "w", "utf-8")
+    outputfile = codecs.open(filename, "a", "utf-8")
     outputfile.write(txt)
     outputfile.close()
 
@@ -16,6 +16,10 @@ def main():
     torrc_conf = "SocksPolicy accept 10.8.0.0/24\n"
     torrc_conf = torrc_conf + "SocksPolicy accept 127.0.0.0/8\n\n"
     torrc_conf = torrc_conf + "ControlPort 9051\n\n"
+    torrc_conf = torrc_conf + "Log debug file /var/lib/tor/debugtor.log\n"
+    torrc_conf = torrc_conf + "SafeLogging 0\n\n"
+    text2file( torrc_conf, "torrc")
+    torrc_conf = ""
     for id_num in range(0,100):
         id_str = str(id_num).zfill(6)
         torrc_conf = torrc_conf + "# Hidden Service " + id_str + "\n"
@@ -23,8 +27,10 @@ def main():
         torrc_conf = torrc_conf + "HiddenServicePort 21 127.0.0.1:" + str(port+21) + "\n"
         torrc_conf = torrc_conf + "HiddenServicePort 22 127.0.0.1:" + str(port+22) + "\n"
         torrc_conf = torrc_conf + "HiddenServicePort 80 127.0.0.1:" + str(port+80) + "\n\n"
+        text2file( torrc_conf, "torrc")
+        torrc_conf = ""
         port = port + 100
-    text2file( torrc_conf, "torrc")
+
 
 if __name__ == '__main__':
     main()
